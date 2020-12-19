fdd3cf6e72789740c4fe1605335afd29b1c5a45c
Owen Stubbs <owen77stubbs@gmail.com>
2020-12-19 03:17:07 +0000

    2 changes, preset positions pretty much done.
    updated each states x and y values
    added setActive and isActive methods

--- src/main/java/net/earthmc/emc/utils/HudUtils.java
+++ src/main/java/net/earthmc/emc/utils/HudUtils.java
@@ -4,23 +4,25 @@ public class HudUtils
 {
     public enum State
     {
-        BOTTOM_LEFT("BOTTOM_LEFT", 5, 375),
-        BOTTOM_MIDDLE("BOTTOM_MIDDLE", 450, 375),
-        BOTTOM_RIGHT("BOTTOM_RIGHT", 770, 375),
-        LEFT("LEFT", 0, 0),
-        RIGHT("RIGHT", 0, 0),
-        TOP_LEFT("TOP_LEFT", 5, 16),
-        TOP_MIDDLE("TOP_MIDDLE", 0, 0),
-        TOP_RIGHT("TOP_RIGHT", 0, 0);
+        BOTTOM_LEFT("BOTTOM_LEFT", 5, 375, false),
+        BOTTOM_MIDDLE("BOTTOM_MIDDLE", 450, 375, false),
+        BOTTOM_RIGHT("BOTTOM_RIGHT", 800, 375, false),
+        LEFT("LEFT", 5, 250, false),
+        RIGHT("RIGHT", 800, 250, false),
+        TOP_LEFT("TOP_LEFT", 5, 16, false),
+        TOP_MIDDLE("TOP_MIDDLE", 450, 16, false),
+        TOP_RIGHT("TOP_RIGHT", 800, 16, false);
 
+        private boolean active;
         private final String name;
         private final int x;
         private final int y;
 
-        State(String name, int x, int y) {
+        State(String name, int x, int y, boolean active) {
             this.name = name;
             this.x = x;
             this.y = y;
+            this.active = active;
         }
 
         public String getName() {
@@ -33,6 +35,14 @@ public class HudUtils
 
         public int getY() {
             return y;
+        }
+
+        public boolean isActive() {
+            return active;
+        }
+
+        public void setActive(boolean a) {
+            active = a;
         }
     }
 }
