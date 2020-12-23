a0bec0b79ba43720633fc6f671471906f07c575c
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2020-12-23 22:37:31 +0100

    Update ClientSideCommands.java

--- src/main/java/net/earthmc/emc/ClientSideCommands.java
+++ src/main/java/net/earthmc/emc/ClientSideCommands.java
@@ -1,59 +1,16 @@
 package net.earthmc.emc;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
-
-import io.github.cottonmc.clientcommands.ArgumentBuilders;
 import io.github.cottonmc.clientcommands.ClientCommandPlugin;
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
+import net.earthmc.emc.commands.NetherCommand;
+import net.earthmc.emc.commands.TownlessCommand;
 
-public class ClientSideCommands implements ClientCommandPlugin 
-{
-
-    @Override
-    public void registerCommands(CommandDispatcher<CottonClientCommandSource> dispatcher)
-    {
-        dispatcher.register(ArgumentBuilders.literal("townless").executes
-        (            
-            source -> 
-            {
-                final JsonArray townless = EMCMod.townless;
-                String townlessString = "";
-                Formatting headingFormatting = Formatting.byName(EMCMod.config.townless.headingTextColour);
-                Formatting playerNameFormattting = Formatting.byName(EMCMod.config.townless.playerTextColour);
-
-                for (int i = 0; i < townless.size(); i++)
-                {
-                    JsonObject currentPlayer = (JsonObject) townless.get(i);
-                    townlessString += currentPlayer.get("name").getAsString() + " ";
-                }
-
-                source.getSource().sendFeedback(new TranslatableText("Townless Players [" + townless.size() + "]").formatted(headingFormatting));
-                source.getSource().sendFeedback(new TranslatableText(townlessString).formatted(playerNameFormattting));
-                return Command.SINGLE_SUCCESS;
-            }
-        ).then(ArgumentBuilders.literal("copyNames").executes
-        (
-            source -> 
-            {
-                    final JsonArray townless = EMCMod.townless;
-                    String townlessString = "";
-
-                    for (int i = 0; i < townless.size(); i++)
-                    {
-                        JsonObject currentPlayer = (JsonObject) townless.get(i);
-                        townlessString += currentPlayer.get("name").getAsString() + " ";
-                    }
-
-                    EMCMod.client.keyboard.setClipboard(townlessString);
-                    source.getSource().sendFeedback(new TranslatableText("Copied townless players to clipboard!"), true);
-                    return Command.SINGLE_SUCCESS;
-            }
-        )));
-    }
+public class ClientSideCommands implements ClientCommandPlugin {
     
+    @Override
+    public void registerCommands(CommandDispatcher<CottonClientCommandSource> dispatcher) {
+        TownlessCommand.register(dispatcher);
+        NetherCommand.register(dispatcher);
+    }
 }
