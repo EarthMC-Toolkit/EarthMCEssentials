5192b13aee4cfd81f185538196937525d58e3220
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2020-12-24 16:39:33 +0100

    Update ClientPlayNetworkHandlerMixin.java

--- src/main/java/net/earthmc/emc/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/earthmc/emc/mixin/ClientPlayNetworkHandlerMixin.java
@@ -47,30 +47,29 @@ public class ClientPlayNetworkHandlerMixin
             @Override
             public void run() 
             {
-                if (EMCMod.config.general.enableMod)
+                if (!EMCMod.config.general.enableMod && EMCMod.townless.size() == 0) return;
+                
+                if (EMCMod.config.townless.enabled) EMCMod.townless = EmcApi.getTownless(); //TODO: Fix townless from being empty sometimes.
+
+                JsonObject resident = EmcApi.getResident(EMCMod.clientName);
+
+                // Resident exists
+                if (resident.has("name"))
                 {
-                    if (EMCMod.config.townless.enabled) EMCMod.townless = EmcApi.getTownless();
-
-                    JsonObject resident = EmcApi.getResident(EMCMod.clientName);
-
-                    // Resident exists
-                    if (resident.has("name"))
+                    if (EMCMod.config.townInfo.enabled)
                     {
-                        if (EMCMod.config.townInfo.enabled)
-                        {
-                            EMCMod.clientTownName = resident.get("town").getAsString();
-                            JsonObject town = EmcApi.getTown(EMCMod.clientTownName);
+                        EMCMod.clientTownName = resident.get("town").getAsString();
+                        JsonObject town = EmcApi.getTown(EMCMod.clientTownName);
 
-                            if (!town.entrySet().isEmpty()) EMCMod.townInfo = town;
-                        }
+                        if (!town.entrySet().isEmpty()) EMCMod.townInfo = town;
+                    }
                         
-                        if (EMCMod.config.nationInfo.enabled)
-                        {
-                            EMCMod.clientNationName = resident.get("nation").getAsString();
-                            JsonObject nation = EmcApi.getNation(EMCMod.clientNationName);
+                    if (EMCMod.config.nationInfo.enabled)
+                    {
+                        EMCMod.clientNationName = resident.get("nation").getAsString();
+                        JsonObject nation = EmcApi.getNation(EMCMod.clientNationName);
 
-                            if (!nation.entrySet().isEmpty()) EMCMod.nationInfo = nation;
-                        }
+                        if (!nation.entrySet().isEmpty()) EMCMod.nationInfo = nation;
                     }
                 }
             }
@@ -81,16 +80,15 @@ public class ClientPlayNetworkHandlerMixin
             @Override
             public void run()
             {
-                if (EMCMod.config.general.enableMod)
-                {
-                    if (EMCMod.config.nearby.enabled) EMCMod.nearby = EmcApi.getNearby(EMCMod.config);
+                if (!EMCMod.config.general.enableMod && EMCMod.nearby.size() == 0) return;
+                
+                if (EMCMod.config.nearby.enabled) EMCMod.nearby = EmcApi.getNearby(EMCMod.config);
 
-                    String serverName = ModUtils.getServerName();
-                    if (serverName.endsWith("earthmc.net") && EMCMod.config.general.emcOnly) EMCMod.shouldRender = true; //Uses endsWith because EMC has 2 valid ip's (earthmc.net & play.earthmc.net)
-                    else if (!serverName.endsWith("earthmc.net") && !EMCMod.config.general.emcOnly) EMCMod.shouldRender = true;
-                    else if (serverName.endsWith("earthmc.net") && EMCMod.config.general.emcOnly) EMCMod.shouldRender = true;
-                    else EMCMod.shouldRender = false;
-                }
+                String serverName = ModUtils.getServerName();
+                if (serverName.endsWith("earthmc.net") && EMCMod.config.general.emcOnly) EMCMod.shouldRender = true; //Uses endsWith because EMC has 2 valid ip's (earthmc.net & play.earthmc.net)
+                else if (!serverName.endsWith("earthmc.net") && !EMCMod.config.general.emcOnly) EMCMod.shouldRender = true;
+                else if (serverName.endsWith("earthmc.net") && EMCMod.config.general.emcOnly) EMCMod.shouldRender = true;
+                else EMCMod.shouldRender = false;
             }
         }, 0, 10 * 1000);
         // #endregion
