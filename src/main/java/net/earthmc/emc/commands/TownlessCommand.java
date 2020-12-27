3f4c031b9b51d16021eb74ceb90645c77d9cb0d2
Owen Stubbs <owen77stubbs@gmail.com>
2020-12-27 23:48:12 +0000

    more error preventions and cleanup

--- src/main/java/net/earthmc/emc/commands/TownlessCommand.java
+++ src/main/java/net/earthmc/emc/commands/TownlessCommand.java
@@ -20,19 +20,19 @@ public class TownlessCommand
             source -> 
             {
                 final JsonArray townless = EMCMod.townless;
-                String townlessString = "";
+                StringBuilder townlessString = new StringBuilder();
                 Formatting headingFormatting = Formatting.byName(EMCMod.config.townless.headingTextColour);
-                Formatting playerNameFormattting = Formatting.byName(EMCMod.config.townless.playerTextColour);
+                Formatting playerNameFormatting = Formatting.byName(EMCMod.config.townless.playerTextColour);
 
                 for (int i = 0; i < townless.size(); i++)
                 {
                     JsonObject currentPlayer = (JsonObject) townless.get(i);
-                    townlessString += currentPlayer.get("name").getAsString() + " ";
+                    townlessString.append(currentPlayer.get("name").getAsString()).append(" ");
                 }
 
                 if (townlessString.length() != 0) {
                     source.getSource().sendFeedback(new TranslatableText("Townless Players [" + townless.size() + "]").formatted(headingFormatting));
-                    source.getSource().sendFeedback(new TranslatableText(townlessString).formatted(playerNameFormattting));
+                    source.getSource().sendFeedback(new TranslatableText(townlessString.toString()).formatted(playerNameFormatting));
                 } else {
                     source.getSource().sendFeedback(new TranslatableText("There don't seem to be any townless players online at the moment.").formatted(Formatting.byName("RED")));
                 }
@@ -44,16 +44,19 @@ public class TownlessCommand
             source -> 
             {
                 final JsonArray townless = EMCMod.townless;
-                String townlessString = "";
+                StringBuilder townlessString = new StringBuilder();
 
                 for (int i = 0; i < townless.size(); i++)
                 {
                     JsonObject currentPlayer = (JsonObject) townless.get(i);
                     if (("/towny:town invite " + townlessString + currentPlayer.get("name").getAsString()).length() > 256) break;
-                    else townlessString += currentPlayer.get("name").getAsString() + " ";
+                    else townlessString.append(currentPlayer.get("name").getAsString()).append(" ");
                 }
 
-                EMCMod.client.player.sendChatMessage("/towny:town invite " + townlessString);
+                if (EMCMod.client.player != null) {
+                    EMCMod.client.player.sendChatMessage("/towny:town invite " + townlessString);
+                }
+
                 source.getSource().sendFeedback(new TranslatableText("EMCE > Invites sent!").formatted(Formatting.byName("AQUA")));
                 source.getSource().sendFeedback(new TranslatableText("EMCE > Note: You still need permissions to invite players to your town.").formatted(Formatting.byName("RED")));
                 return Command.SINGLE_SUCCESS;
