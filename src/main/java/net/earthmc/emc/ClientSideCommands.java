10d48253ec20f5b741c2ce2188d9a1e37fb4d21c
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-01-04 23:57:31 +0100

    Update ClientSideCommands.java

--- src/main/java/net/earthmc/emc/ClientSideCommands.java
+++ src/main/java/net/earthmc/emc/ClientSideCommands.java
@@ -3,6 +3,7 @@ package net.earthmc.emc;
 import com.mojang.brigadier.CommandDispatcher;
 import io.github.cottonmc.clientcommands.ClientCommandPlugin;
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
+import net.earthmc.emc.commands.InfoCommands;
 import net.earthmc.emc.commands.NetherCommand;
 import net.earthmc.emc.commands.QueueCommand;
 import net.earthmc.emc.commands.TownlessCommand;
@@ -14,5 +15,7 @@ public class ClientSideCommands implements ClientCommandPlugin {
         TownlessCommand.register(dispatcher);
         NetherCommand.register(dispatcher);
         QueueCommand.register(dispatcher);
+        InfoCommands.registerNationInfoCommand(dispatcher);
+        InfoCommands.registerTownInfoCommand(dispatcher);
     }
 }
