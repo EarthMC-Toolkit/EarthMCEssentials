c4aa77d6977d7ee1f9046c41bc08cbd90b69ce39
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-04 13:57:18 +0100

    Simplify the api

--- src/main/java/net/emc/emce/utils/EmcApi.java
+++ src/main/java/net/emc/emce/utils/EmcApi.java
@@ -11,219 +11,72 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-public class EmcApi
-{
-    public static JsonArray getTownless()
-    {
-        try
-        {
-            final URL url = new URL("http://earthmc-api.herokuapp.com/townlessplayers");
-
-            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            // Getting the response code
-            final int responsecode = conn.getResponseCode();
-
-            if (responsecode == 200)
-            {
-                StringBuilder inline = new StringBuilder();
-                final Scanner scanner = new Scanner(url.openStream());
-
-                // Write all the JSON data into a string using a scanner
-                while (scanner.hasNext())
-                {
-                    inline.append(scanner.nextLine());
-                }
-
-                // Close the scanner
-                scanner.close();
-
-                // Using the JSON simple library parse the string into a json object
-                final JsonParser parse = new JsonParser();
-                return (JsonArray) parse.parse(inline.toString());
-            }
-        }
-        catch (Exception ignore) {
+public class EmcApi {
+    public static JsonArray getTownless() {
+        try {
+            return (JsonArray) new JsonParser().parse(getURL("http://earthmc-api.herokuapp.com/townlessplayers").toString());
+        } catch (Exception e) {
             return EMCE.townless;
         }
-
-        return EMCE.townless;
     }
 
-    public static JsonArray getNearby(int xBlocks, int zBlocks)
-    {
-        try
-        {
+    public static JsonArray getNearby(int xBlocks, int zBlocks) {
+        try {
             MinecraftClient client = MinecraftClient.getInstance();
             ClientPlayerEntity player = client.player;
-            final URL url;
+            
+            if (player != null) {
+               JsonArray array = (JsonArray) new JsonParser().parse(getURL("http://earthmc-api.herokuapp.com/nearby/" + (int) player.getX() + "/" + (int) player.getZ() + "/" + xBlocks + "/" + zBlocks).toString());
 
-            if (player != null)
-            {
-                url = new URL("http://earthmc-api.herokuapp.com/nearby/" + (int) player.getX() + "/" + (int) player.getZ() + "/" + xBlocks + "/" + zBlocks);
-
-                final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.connect();
-
-                // Getting the response code
-                final int responsecode = conn.getResponseCode();
-
-                if (responsecode == 200) {
-                    StringBuilder inline = new StringBuilder();
-                    final Scanner scanner = new Scanner(url.openStream());
-
-                    // Write all the JSON data into a string using a scanner
-                    while (scanner.hasNext()) {
-                        inline.append(scanner.nextLine());
-                    }
-
-                    // Close the scanner
-                    scanner.close();
-
-                    // Using the JSON simple library parse the string into a json object
-                    final JsonParser parse = new JsonParser();
-                    JsonArray array = (JsonArray) parse.parse(inline.toString());
-
-                    for (int i = 0; i < array.size(); i++) {
-                        JsonObject currentObj = (JsonObject) array.get(i);
-                        if (currentObj.get("name").getAsString().equals(client.player.getName().asString())) array.remove(i);
-                    }
-                    return array;
+                for (int i = 0; i < array.size(); i++) {
+                    JsonObject currentObj = (JsonObject) array.get(i);
+                    if (currentObj.get("name").getAsString().equals(client.player.getName().asString())) array.remove(i);
                 }
-            }
+                return array;
+            } else
+                return EMCE.nearby;
         }
         catch (Exception ignore) {
             return EMCE.nearby;
-        }
-
-        return EMCE.nearby;
+        }        
     }
 
-    public static JsonObject getResident(String residentName)
-    {
-        try
-        {
-            final URL url = new URL("http://earthmc-api.herokuapp.com/residents/" + residentName);
-
-            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            // Getting the response code
-            final int responsecode = conn.getResponseCode();
-
-            if (responsecode == 200)
-            {
-                StringBuilder inline = new StringBuilder();
-                final Scanner scanner = new Scanner(url.openStream());
-
-                // Write all the JSON data into a string using a scanner
-                while (scanner.hasNext())
-                {
-                    inline.append(scanner.nextLine());
-                }
-
-                // Close the scanner
-                scanner.close();
-
-                // Using the JSON simple library parse the string into a json object
-                final JsonParser parse = new JsonParser();
-                return (JsonObject) parse.parse(inline.toString());
-            }
-        }
-        catch (Exception ignore) {
+    public static JsonObject getResident(String residentName) {
+        try {
+            return (JsonObject) new JsonParser().parse(getURL("http://earthmc-api.herokuapp.com/residents/" + residentName).toString());
+        } catch (Exception e) {
             return new JsonObject();
         }
-
-        return new JsonObject();
     }
 
-    public static JsonArray getTowns()
-    {
-        try
-        {
-            final URL url = new URL("http://earthmc-api.herokuapp.com/towns/");
-
-            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            // Getting the response code
-            final int responsecode = conn.getResponseCode();
-
-            if (responsecode == 200)
-            {
-                StringBuilder inline = new StringBuilder();
-                final Scanner scanner = new Scanner(url.openStream());
-
-                // Write all the JSON data into a string using a scanner
-                while (scanner.hasNext())
-                {
-                    inline.append(scanner.nextLine());
-                }
-
-                // Close the scanner
-                scanner.close();
-
-                // Using the JSON simple library parse the string into a json object
-                final JsonParser parse = new JsonParser();
-                return (JsonArray) parse.parse(inline.toString());
-            }
-        }
-        catch (Exception ignore)
-        {
-            return new JsonArray();
-        }
-
-        return new JsonArray();
+    public static JsonArray getTowns() {
+        try {
+            return (JsonArray) new JsonParser().parse(getURL("http://earthmc-api.herokuapp.com/towns/").toString());
+        } catch (Exception e) {
+            return EMCE.allTowns;
+        }        
     }
 
-    public static JsonObject getServerInfo() 
-    {
-        try 
-        {
-            final URL url = new URL("http://earthmc-api.herokuapp.com/serverinfo/");
-
-            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            // Getting the response code
-            final int responsecode = conn.getResponseCode();
-
-            if (responsecode == 200)
-            {
-                StringBuilder inline = new StringBuilder();
-                final Scanner scanner = new Scanner(url.openStream());
-
-                // Write all the JSON data into a string using a scanner
-                while (scanner.hasNext())
-                {
-                    inline.append(scanner.nextLine());
-                }
-
-                // Close the scanner
-                scanner.close();
-
-                // Using the JSON simple library parse the string into a json object
-                final JsonParser parse = new JsonParser();
-                return (JsonObject) parse.parse(inline.toString());
-            }
-        }
-        catch (Exception ignore) {
+    public static JsonObject getServerInfo() {
+        try {
+            return (JsonObject) new JsonParser().parse(getURL("http://earthmc-api.herokuapp.com/serverinfo/").toString());
+        } catch (Exception e) {
             return new JsonObject();
-        } 
-        return new JsonObject();
+        }
     }
 
     public static JsonArray getNations() {
         try {
-            final URL url = new URL("http://earthmc-api.herokuapp.com/nations/");
+            return (JsonArray) new JsonParser().parse(getURL("http://earthmc-api.herokuapp.com/nations/").toString());
+        } catch (Exception e) {
+            return EMCE.allNations;
+        }
+    }
 
-            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    private static StringBuilder getURL(String urlString) throws Exception {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
 
@@ -236,12 +89,11 @@ public class EmcApi
 
                 scanner.close();
 
-                final JsonParser parse = new JsonParser();
-                return (JsonArray) parse.parse(inline.toString());
-            }
-        } catch (Exception ignore) {
-            return new JsonArray();
+                return inline;
+            } else
+                throw new Exception("Invalid response code when getting URL");
+        } catch (Exception e) {
+            return new StringBuilder();
         }
-        return new JsonArray();
     }
 }
