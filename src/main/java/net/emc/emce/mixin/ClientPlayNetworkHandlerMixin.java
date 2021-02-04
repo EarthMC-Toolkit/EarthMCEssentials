a6c46584be53d0fa1d265c84e99f0bd80a192799
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-04 15:10:58 +0100

    Send message when voxelmap gets disabled

--- src/main/java/net/emc/emce/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/emc/emce/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,11 +1,13 @@
 package net.emc.emce.mixin;
 
 import net.emc.emce.EMCE;
+import net.emc.emce.PlayerMessaging;
 import net.emc.emce.utils.ModUtils;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.Formatting;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,7 +30,8 @@ public class ClientPlayNetworkHandlerMixin {
         EMCE.shouldRender = shouldRender;
 
         if (shouldRender && config.general.disableVoxelMap && !client.isInSingleplayer() && FabricLoader.getInstance().isModLoaded("voxelmap") && config.general.enableMod) {
-            client.player.sendMessage(new LiteralText("§3 §6 §3 §6 §3 §6 §d§3 §6 §3 §6 §3 §6 §e"), false);
+            PlayerMessaging.sendMessage("msg_voxelmap_disabled", Formatting.BLUE, true);
+            client.player.sendMessage(new LiteralText("§3 §6 §3 §6 §3 §6 §d§3 §6 §3 §6 §3 §6 §e"), false);  
         }
 
         // If the timers aren't running, start them.
