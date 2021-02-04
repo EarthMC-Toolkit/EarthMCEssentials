1d30dd3251adbe50c82ff606ba96e43c1f7cef72
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-04 13:56:51 +0100

    Implement disablevoxelmap option

--- src/main/java/net/emc/emce/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/emc/emce/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,7 +1,11 @@
 package net.emc.emce.mixin;
 
+import net.emc.emce.EMCE;
+import net.emc.emce.utils.ModUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.text.LiteralText;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,13 +15,20 @@ import static net.emc.emce.EMCE.*;
 import static net.emc.emce.utils.Timers.*;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public class ClientPlayNetworkHandlerMixin
-{
+public class ClientPlayNetworkHandlerMixin {
     @Inject(at = @At("TAIL"), method="onGameJoin")
-    private void onGameJoin(CallbackInfo info)
-    {
+    private void onGameJoin(CallbackInfo info) {
         client = MinecraftClient.getInstance();
-        if (client.player != null) clientName = client.player.getName().asString();
+        if (client.player != null) {
+            clientName = client.player.getName().asString();
+        }
+
+        boolean shouldRender = ModUtils.shouldRender();
+        EMCE.shouldRender = shouldRender;
+
+        if (shouldRender && config.general.disableVoxelMap && !client.isInSingleplayer()) {
+            client.player.sendMessage(new LiteralText("§3 §6 §3 §6 §3 §6 §d§3 §6 §3 §6 §3 §6 §e"), false);
+        }
 
         // If the timers aren't running, start them.
         if (!getRunning()) startAll();
