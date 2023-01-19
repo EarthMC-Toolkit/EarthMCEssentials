54510793aa038203bae9147e9d5b08620df46864
Owen Stubbs <owen77stubbs@gmail.com>
2023-01-19 15:18:30 +0000

    add try catch to config open logic

--- src/main/java/net/emc/emce/modules/EventRegistry.java
+++ src/main/java/net/emc/emce/modules/EventRegistry.java
@@ -6,6 +6,7 @@ import net.emc.emce.EarthMCEssentials;
 import net.emc.emce.config.ModConfig;
 import net.emc.emce.events.commands.*;
 import net.emc.emce.events.screen.ScreenInit;
+import net.emc.emce.utils.Messaging;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
@@ -28,13 +29,15 @@ public class EventRegistry {
         // Every tick, see if we are pressing F4.
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (EarthMCEssentials.configKeybinding.wasPressed()) {
-                boolean open = ScreenInit.configOpen();
+                if (!ScreenInit.configOpen()) {
+                    try {
+                        Screen configScreen = AutoConfig.getConfigScreen(ModConfig.class, client.currentScreen).get();
+                        client.setScreen(configScreen);
 
-                if (!open) {
-                    ScreenInit.setConfigOpen(true);
-
-                    Screen configScreen = AutoConfig.getConfigScreen(ModConfig.class, client.currentScreen).get();
-                    client.setScreen(configScreen);
+                        ScreenInit.setConfigOpen(true);
+                    } catch (Exception e) {
+                        Messaging.sendDebugMessage("Error setting screen!", e);
+                    }
                 }
             }
         });
